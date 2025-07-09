0374b6f30d6f2001be3569af89d701b01f0e217d
Kamilla <kamillabitencourt18@gmail.com>
2025-07-09 02:42:28 -0300

    Corrigido router para HashRouter no App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AppRoutes from "./routes";
 
-
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <AppRoutes />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
